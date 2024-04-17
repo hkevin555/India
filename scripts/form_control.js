397272ffa670f0e05670c66680ebc8b3c38240e0
hkevin555 <157328999+hkevin555@users.noreply.github.com>
2024-04-17 14:14:20 -0500

    Add files via upload

---
+++ scripts/form_control.js
@@ -0,0 +1,17 @@
+function main() {
+  // data for each form field
+  data = ["hkevin@hotmail.com",
+    "(256)-999-9999",
+    "Kevin Hernandez",
+    "1234 Duck Ave Florence, AL 35631",
+    "0",
+    "0",
+    "0",
+    "loved it would definitley come back again",
+    "1"]
+    
+  jquery_functions(data);
+}
+
+//run program
+main();
